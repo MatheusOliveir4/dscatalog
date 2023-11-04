5193a8c2c4c2bf3046c4b5d452c1d177bda7f4e8
MatheusOliveir4 <matheusfloliveira@gmail.com>
2023-11-04 16:13:16 -0300

    CategoryResource

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dscatalog.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
   @EqualsAndHashCode.Include
